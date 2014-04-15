ed591abdaff2f2a5cbba1d10aa46ac5e2a417871
Yegor Bugayenko <yegor@tpc2.com>
2014-04-15 13:34:00 +0200

    travis config

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,10 @@ SRC=$(pwd)
 rm -rf _site
 jekyll build
 
-CLONE=$(mktemp -d -t blog)
-git clone git@github.com:yegor256/blog.git ${CLONE}
-cd ${CLONE}
+mkdir -p ~/.ssh
+CLONE=$(mktemp -d -t blog-XXX)
+git clone "https://yegor256:${PASSWORD}@github.com/yegor256/blog.git" "${CLONE}"
+cd "${CLONE}"
 git checkout gh-pages
 rm -rf *
 cp -R ${SRC}/_site/* .
@@ -14,7 +15,9 @@ rm README.md
 rm deploy.sh
 
 git add .
-git commit -am "new site version"
-git push origin gh-pages
+git config --global user.email "deploy@timed.io"
+git config --global user.name "deploy.sh"
+git commit -am "new site version deployed"
+git push --quiet origin gh-pages
 
-rm -rf ${CLONE}
+rm -rf "${CLONE}"
