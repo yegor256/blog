7bbdc0fb7bca35eff01dd3a971c56e87d07112eb
Yegor Bugayenko <yegor@tpc2.com>
2014-06-11 09:27:53 +0300

    emails

--- _newsletters/send.rb
+++ _newsletters/send.rb
@@ -14,12 +14,30 @@ Usage: send.rb [options]
   opt :password, 'SMTP password', :type=>String, :required=>true
   opt :file, 'Absolute path to the list of emails', :type=>String, :required=>true
   opt :subject, 'Email subject', :type=>String, :required=>true
+  opt :body, 'Email body file', :type=>String, :required=>true
 end
 
 host = opts[:host]
 puts "Sending emails to #{opts[:host]}:#{opts[:port]} as #{opts[:user]}"
 
+require 'mail'
+require 'uuidtools'
+
+body = File.read(opts[:body])
+
+Mail.defaults do
+  delivery_method :smtp, address: opts[:host], port: opts[:port]
+end
+
 File.readlines(opts[:file]).collect(&:strip).each do |email|
   print "  sending to #{email}..."
+  mail = Mail.deliver do
+    from 'me@yegor256.com'
+    to email
+    message_id "<#{UUIDTools::UUID.random_create}@yegor256.com>"
+    subject opts[:subject]
+    body body
+  end
+  puts mail.to_s
   puts ' done'
 end
