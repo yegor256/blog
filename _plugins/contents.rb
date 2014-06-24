a1a5c2860938fdb36d29e8d454e1d757e164a8fa
Yegor Bugayenko <yegor@tpc2.com>
2014-06-24 11:54:33 +0200

    #master: contents.html

--- _plugins/contents.rb
+++ _plugins/contents.rb
@@ -1,30 +1,22 @@
-require 'fileutils'
-
 module Yegor
-  class Contents < Jekyll::Generator
-    priority :low
-
-    def generate(site)
-      html = '<html><body>'
+  class ContentsBlock < Liquid::Tag
+    def render(context)
       year = ''
       month = ''
-      site.posts.reverse.each do |post|
+      html = ''
+      context['site']['posts'].each do |post|
         if post.date.year != year
           year = post.date.year
-          html += "<h1>#{year}</h1>"
         end
         if post.date.month != month
           month = post.date.month
-          html += "<h2>#{month}</h2>"
+          html += "<h2>#{Date::MONTHNAMES[month]} #{year}</h2>"
         end
         html += "<p><a href='#{post.url}'>#{post.title}</a></p>"
       end
-      html += '</body></html>'
-      path = File.join(site.dest, 'contents.html')
-      FileUtils.mkdir_p(File.dirname(path))
-      File.open(path, 'w') { |f| f.write(html) }
-      site.pages << Jekyll::Page.new(site, site.dest, '', File.basename(path))
+      html
     end
-
   end
 end
+
+Liquid::Template.register_tag("contents", Yegor::ContentsBlock)
