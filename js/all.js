309db18dac604d24ca236a40f4dca0ee47d87e1b
Yegor Bugayenko <yegor@tpc2.com>
2014-08-03 22:55:12 +0200

    script simplified

--- js/all.js
+++ js/all.js
@@ -171,15 +171,3 @@ var disqus_shortname = 'yegor256';
   dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 })();
-
-// Google Custom Search Engine
-(function() {
-  var cx = '012237142075217162703:vly1vck9bom';
-  var gcse = document.createElement('script');
-  gcse.type = 'text/javascript';
-  gcse.async = true;
-  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-    '//www.google.com/cse/cse.js?cx=' + cx;
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(gcse, s);
-})();
