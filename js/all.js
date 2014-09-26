c035a193fe043638eb14c74b08d9a205eb8cbc27
Yegor Bugayenko <yegor@tpc2.com>
2014-09-26 19:24:17 +0200

    fadeIn

--- js/all.js
+++ js/all.js
@@ -43,25 +43,25 @@ $(
       'http://free.sharedcount.com/?apikey=d730c518430eabcabc46ab79528c744067afa17e&url=' + encodeURIComponent(document.location.href),
       function (data) {
         if (data.Facebook.total_count !== 0) {
-          $('.count-facebook').html(data.Facebook.total_count).show();
+          $('.count-facebook').html(data.Facebook.total_count).fadeIn();
         }
         if (data.Twitter !== 0) {
-          $('.count-twitter').html(data.Twitter).show();
+          $('.count-twitter').html(data.Twitter).fadeIn();
         }
         if (data.GooglePlusOne !== 0) {
-          $('.count-googleplus').html(data.GooglePlusOne);
+          $('.count-googleplus').html(data.GooglePlusOne).fadeIn();
         }
         if (data.LinkedIn !== 0) {
-          $('.count-linkedin').html(data.LinkedIn).show();
+          $('.count-linkedin').html(data.LinkedIn).fadeIn();
         }
         if (data.Reddit !== 0) {
-          $('.count-reddit').html(data.Reddit);
+          $('.count-reddit').html(data.Reddit).fadeIn();
         }
         if (data.StumbleUpon !== 0) {
-          $('.count-stumbleupon').html(data.StumbleUpon);
+          $('.count-stumbleupon').html(data.StumbleUpon).fadeIn();
         }
         if (data.Diggs !== 0) {
-          $('.count-digg').html(data.Diggs).show();
+          $('.count-digg').html(data.Diggs).fadeIn();
         }
       }
     );
