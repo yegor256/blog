d834a91f3cdaad66037121896346f27f8404c4d0
Yegor Bugayenko <yegor@tpc2.com>
2014-10-02 17:41:48 +0200

    no-hash for fontcustom

--- _plugins/icons.rb
+++ _plugins/icons.rb
@@ -1,3 +1,4 @@
+require 'securerandom'
 module Jekyll
   class FontFile < StaticFile
     def write(dest)
@@ -18,12 +19,18 @@ module Jekyll
       scss = '_ico.scss'
       system(
         "fontcustom compile _glyphs --output=_site/#{dir} \
-        --font-name=#{font} --templates=scss --force \
+        --font-name=#{font} --templates=scss --no-hash --force \
         --autowidth --preprocessor-path=/#{dir} \
         && mkdir -p #{temp} && mv _site/#{dir}/_#{font}.scss #{temp}/#{scss}"
       )
       file = "#{temp}/#{scss}";
-      File.write(file, File.read(file).gsub(/font-url\((.*)\)/, 'url(\1)'))
+      File.write(
+        file,
+        File.read(file).gsub(
+          /font-url\((.*)\)/,
+          "url(\\1?#{SecureRandom.urlsafe_base64(6)})"
+        )
+      )
       ['ttf', 'svg', 'woff', 'eot'].each { |ext|
         site.static_files << Jekyll::FontFile.new(site, site.dest, '/', "#{dir}/#{font}.#{ext}")
       }
