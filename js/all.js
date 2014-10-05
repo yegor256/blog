3f17f703b3b75356e785ae98db281c45c2b5accf
Yegor Bugayenko <yegor@tpc2.com>
2014-10-05 08:48:54 +0200

    metrica js fix

--- js/all.js
+++ js/all.js
@@ -242,21 +242,21 @@ var disqus_shortname = 'yegor256';
 (function (d, w, c) {
   'use strict';
   (w[c] = w[c] || []).push(function() {
-    try {
-      w.yaCounter26361360 = new Ya.Metrika({id:26361360,
-        webvisor:true,
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true});
-    } catch(e) { }
+  try {
+    w.yaCounter26361360 = new Ya.Metrika({id:26361360,
+    webvisor:true,
+    clickmap:true,
+    trackLinks:true,
+    accurateTrackBounce:true});
+  } catch(e) { }
   });
   var n = d.getElementsByTagName("script")[0],
-    s = d.createElement("script"),
-    f = function () { n.parentNode.insertBefore(s, n); };
+  s = d.createElement("script"),
+  f = function () { n.parentNode.insertBefore(s, n); };
   s.type = "text/javascript";
   s.async = true;
   s.src = (d.location.protocol === "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
   if (w.opera === "[object Opera]") {
-    d.addEventListener("DOMContentLoaded", f, false);
+  d.addEventListener("DOMContentLoaded", f, false);
   } else { f(); }
 }(document, window, "yandex_metrika_callbacks"));
