9b2ca8c7b64463509e47fb369376174563572e13
Yegor Bugayenko <yegor@tpc2.com>
2014-10-07 09:56:07 +0200

    renamed

--- _test/run.sh
+++ _test/run.sh
@@ -5,6 +5,7 @@ DIR=$(dirname $0)
 PORT=4000
 jekyll serve --port=$PORT --host=localhost "--source=$DIR/.." &
 PID=$!
+COUNT=1
 echo "Jekyll started, PID: ${PID}"
 while true; do
   status=$(curl --silent --write-out "%{http_code}" http://localhost:$PORT --output /dev/stderr 2>/dev/null | cat)
@@ -14,6 +15,11 @@ while true; do
   fi
   echo "waiting for Jekyll to get ready..."
   sleep 15
+  (( COUNT++ ))
+  if [ $count == 5 ]; then
+    echo "Jekyll takes too long"
+    break
+  fi
 done
 casperjs test $DIR/casperjs/*
 kill -9 $PID
