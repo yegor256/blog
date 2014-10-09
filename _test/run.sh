a6e81a016024c3134e923f5b43ad69f2712e8c27
Yegor Bugayenko <yegor@tpc2.com>
2014-10-09 11:34:04 +0200

    random URL for reddit

--- _test/run.sh
+++ _test/run.sh
@@ -5,23 +5,24 @@ DIR=$(dirname $0)
 PORT=4000
 jekyll serve --port=$PORT --host=localhost "--source=$DIR/.." &
 PID=$!
+trap 'kill -9 $PID; rm -rf $DIR/../_site' EXIT
+
 COUNT=1
 echo "Jekyll started, PID: ${PID}"
 while true; do
-  status=$(curl --silent --write-out "%{http_code}" http://localhost:$PORT --output /dev/stderr 2>/dev/null | cat)
-  if [ $status -eq 200 ]; then
+  STATUS=$(curl --silent --write-out "%{http_code}" http://localhost:$PORT --output /dev/stderr 2>/dev/null | cat)
+  if [ $STATUS -eq 200 ]; then
     echo "Jekyll is ready!"
     break
   fi
-  echo "waiting for Jekyll to get ready..."
+  echo "waiting for Jekyll to get ready (${COUNT})..."
   sleep 15
   (( COUNT++ ))
-  if [ $count == 5 ]; then
+  if [ $COUNT -eq 5 ]; then
     echo "Jekyll takes too long"
     break
   fi
 done
+
 casperjs test $DIR/casperjs/*
-kill -9 $PID
-echo "Jekyll stopped"
-rm -rf $DIR/../_site
+find . -name '*.html' -exec echo {} \;
