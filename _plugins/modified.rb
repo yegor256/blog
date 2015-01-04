bdf0e22990e744e01739c2e45189bd01f6153827
Yegor Bugayenko <yegor@tpc2.com>
2015-01-04 12:06:34 +0100

    modified fix

--- _plugins/modified.rb
+++ _plugins/modified.rb
@@ -5,24 +5,11 @@ module Yegor
       path = page['path']
       count = `git rev-list HEAD --count #{path}`.strip.to_i
       if count > 1
-        date = Date.parse(`git log --format=%aI #{path}`.strip)
-        "<li><a href='https://github.com/yegor256/blog/commits/master/#{path}'>#{order(count)}</a>\
-          modification on\
-          <time itemprop='dateModified' datetime='#{date.strftime('%Y-%m-%dT%H:%M:%S%z')}'>#{date.strftime('%-d %B %Y')}</time></li>"
-      else
-        ''
-      end
-    end
-    private
-    def order(count)
-      if count == 1
-        'one'
-      elsif count == 2
-        'second'
-      elsif count == 3
-        'third'
-      else
-        "#{count}th"
+        date = Time.parse(`git log --format=%aI #{path}`.strip)
+        if date > page['date']
+          "<li><a href='https://github.com/yegor256/blog/commits/master/#{path}'>modified</a>\
+          on <time itemprop='dateModified' datetime='#{date.strftime('%Y-%m-%dT%H:%M:%S%z')}'>#{date.strftime('%-d %B %Y')}</time></li>"
+        end
       end
     end
   end
