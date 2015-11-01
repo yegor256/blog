89329d81cbe29556131a5e5adab6665b4b5d1d3c
Yegor Bugayenko <yegor@tpc2.com>
2015-11-01 13:58:26 -0800

    show errors

--- _test/test_orphans.sh
+++ _test/test_orphans.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
+xmllint -version
+tidy -version
+
 mkdir -p _temp
 rm -rf _temp/links.txt
 for f in $(find . -regex '.*_site/[0-9][0-9][0-9][0-9]/.*\.html'); do
   echo -n "fetching links from $f... "
   tidy -i -asxml $f 2>/dev/null | \
-    xmllint -html -xpath '//article//a[starts-with(@href,"/")]/@href' - 2>/dev/null | \
+    xmllint -html -xpath '//article//a[starts-with(@href,"/")]/@href' - | \
     sed 's|href="\([^"]\+\)"|\1|g' | \
     sed "s| |\n|g" >> _temp/links.txt
   echo "" >> _temp/links.txt
