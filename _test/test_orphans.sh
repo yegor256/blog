942129abe009a1a3acd52ca72dccea255b7bfe51
Yegor Bugayenko <yegor@tpc2.com>
2015-11-01 12:00:01 -0800

    more logs

--- _test/test_orphans.sh
+++ _test/test_orphans.sh
@@ -4,12 +4,14 @@ set -e
 mkdir -p _temp
 rm -rf _temp/links.txt
 for f in $(find . -regex '.*_site/[0-9]\{4\}/.*\.html'); do
+  echo -n "fetching links from $f... "
   tidy -i -asxml $f 2>/dev/null | \
     xmllint -html -xpath '//article//a[starts-with(@href,"/")]/@href' - 2>/dev/null | \
     sed "s/href=\"\([^\"]*\)\"/\1/g" | \
     sed "s/ /\n/g" >> _temp/links.txt
   echo "" >> _temp/links.txt
   echo $f | sed 's/\.\/_site//g' >> _temp/links.txt
+  echo "OK"
 done
 
 cat _temp/links.txt | \
