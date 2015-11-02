3d10c2c64f68ddde348789c9ffacbc2c1149fcca
Yegor Bugayenko <yegor@tpc2.com>
2015-11-02 15:47:30 -0800

    test scripts reanmed

--- _test/test_links.sh
+++ _test/test_links.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 function ping_uri {
   curl --location --silent --output /dev/null --fail \
@@ -12,11 +11,19 @@ function ping_uri {
 }
 export -f ping_uri
 mkdir -p _temp
-find _site -name '*.html' \
-  | xargs -P 10 -n 1 ruby _test/fetch_links.rb \
-  | sort | uniq \
-  | grep -v 'http://www.yegor256.com' \
-  | xargs -P 10 -n 1 /bin/bash -c 'ping_uri "$0" >> _temp/pings.txt'
+for f in $(find . -regex '.*_site/.*\.html'); do
+  echo -n "fetching links from $f... "
+  tidy -i -asxml $f 2>/dev/null | \
+    xmllint -html -xpath '//article//a/@href' - 2>/dev/null | \
+    sed 's|href="\([^"]\+\)"|\1|g' | \
+    sed "s| |\n|g" >> _temp/links.txt
+  echo "" >> _temp/links.txt
+  echo "OK"
+done
+
+cat _temp/links.txt | uniq | \
+  grep -v 'http://www.yegor256.com' | \
+  xargs -P 10 -n 1 /bin/bash -c 'ping_uri "$0" >> _temp/pings.txt'
 
 cat _temp/pings.txt | grep -v '^200 ' > _temp/broken.txt
 broken=$(cat _temp/broken.txt | wc -l | cut -f1 -d ' ')
