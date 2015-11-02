d3f2b0686daa382971f07bfb39da436baaaad94e
Yegor Bugayenko <yegor@tpc2.com>
2015-11-01 19:34:38 -0800

    typo

--- _test/test_keywords.sh
+++ _test/test_keywords.sh
@@ -12,10 +12,10 @@ for f in $(find _site -name '*.html'); do
     sed "s/^[a-z]\{,3\}$/ /g" >> _temp/words.txt
 done
 
-cat _temp/words.txt | sed '/^[ \t]*$/d' | sort > _temp/sorted-words.txt
+cat _temp/words.txt | sed '/^[ \t]*$/d' | \
+  sort | uniq -c | sort -r | cut -c9- > _temp/sorted-words.txt
 
-comm -23 _temp/sorted-words.txt _test/stop-words.txt | \
-  sort | uniq -c | sort -r | cut -c9- > _temp/top-words.txt
+comm -23 _temp/sorted-words.txt _test/stop-words.txt | head -200 > _temp/top-words.txt
 
 while read k; do
   echo "${k}:"
