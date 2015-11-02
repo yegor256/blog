654e34f680c35b6ba3e1dc90e003351a0ffad5cd
Yegor Bugayenko <yegor@tpc2.com>
2015-11-01 19:52:43 -0800

    export

--- _test/test_keywords.sh
+++ _test/test_keywords.sh
@@ -2,6 +2,7 @@
 
 mkdir -p _temp
 rm -rf _temp/words.txt
+echo -n "fetching all words... "
 for f in $(find _site -name '*.html'); do
   tidy -i -asxml $f 2>/dev/null | \
     sed "s|>|>\n|g" | \
@@ -9,17 +10,30 @@ for f in $(find _site -name '*.html'); do
     sed "s/[^a-zA-Z]/ /g" | \
     tr A-Z a-z | \
     sed 's/[ \t\n]/\n/g' | \
-    sed "s/^[a-z]\{,3\}$/ /g" >> _temp/words.txt
+    sed '/^[ \t]*$/d' >> _temp/words.txt
 done
+echo -e "done, $(wc -l _temp/words.txt | cut -f1 -d ' ') words found"
 
-cat _temp/words.txt | sed '/^[ \t]*$/d' | \
-  sort | uniq -c | sort -r | cut -c9- > _temp/sorted-words.txt
+rm -rf _temp/good-words.txt
+echo -n "removing stop words... "
+cat _temp/words.txt | sort | uniq -c | sort -r | cut -c9- | \
+while read k; do
+  grep -q $k _test/stop-words.txt
+  if [[ $? != 0 ]]; then
+    echo $k >> _temp/good-words.txt
+  fi
+done
+echo "done, $(wc -l _temp/good-words.txt | cut -f1 -d ' ') good words left"
 
-comm -23 _temp/sorted-words.txt _test/stop-words.txt | head -200 > _temp/top-words.txt
+cat _temp/good-words.txt | head -200 > _temp/top-words.txt
 
 while read k; do
-  echo "${k}:"
-  grep $k _temp/top-words.txt
-  if [[ $? = 1 ]]; then exit -1; fi
+  echo -n "${k}: "
+  grep -q $k _temp/top-words.txt
+  if [[ $? = 1 ]]; then
+    echo "is absent!"
+    exit -1
+  fi
+  echo "OK"
 done < _test/core-keywords.txt
 
