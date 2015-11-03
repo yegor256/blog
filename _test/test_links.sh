295851a6499c562480d1f41a37d3932e3bf43d9f
Yegor Bugayenko <yegor@tpc2.com>
2015-11-02 19:39:37 -0800

    test links

--- _test/test_links.sh
+++ _test/test_links.sh
@@ -10,24 +10,32 @@ function ping_uri {
     --write-out '%{http_code} %{url_effective}\n' $1
 }
 export -f ping_uri
+
 mkdir -p _temp
+rm _temp/links.txt
 for f in $(find . -regex '.*_site/.*\.html'); do
-  echo -n "fetching links from $f... "
   tidy -i -asxml $f 2>/dev/null | \
-    xmllint -html -xpath '//article//a/@href' - 2>/dev/null | \
+    xmllint -html -xpath '//article//a[starts-with(@href,"http")]/@href' - 2>/dev/null | \
     sed 's|href="\([^"]\+\)"|\1|g' | \
     sed "s| |\n|g" >> _temp/links.txt
   echo "" >> _temp/links.txt
-  echo "OK"
 done
 
-cat _temp/links.txt | uniq | \
-  grep -v 'http://www.yegor256.com' | \
-  xargs -P 10 -n 1 /bin/bash -c 'ping_uri "$0" >> _temp/pings.txt'
+cat _temp/links.txt | \
+  uniq | \
+  tr A-Z a-z | \
+  sed '/^[ \t]*$/d' | \
+  grep -v 'http://www.yegor256.com' > _temp/unique-links.txt
+
+total=$(wc -l _temp/unique-links.txt | cut -f1 -d ' ')
+echo "${total} unique foreign links found"
+
+cat _temp/unique-links.txt | xargs -P 10 -n 1 /bin/bash -c 'ping_uri "$0" >> _temp/pings.txt'
 
 cat _temp/pings.txt | grep -v '^200 ' > _temp/broken.txt
 broken=$(cat _temp/broken.txt | wc -l | cut -f1 -d ' ')
 if [ "$broken" -gt "20" ]; then
+  echo "${broken} link(s) broken (out of ${total}):"
   cat _temp/broken.txt
   exit -1
 fi
