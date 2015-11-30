b24d3bf48e4a5ce6f010b79db076778da24b9808
Yegor Bugayenko <yegor@tpc2.com>
2015-11-30 14:25:52 +0200

    new converter

--- _newsletters/convert-twitter-subscribers.rb
+++ _newsletters/convert-twitter-subscribers.rb
@@ -1,7 +1,37 @@
 #!/usr/bin/env ruby
 STDOUT.sync = true
 
-ARGF.each_line do |line|
+emails = []
+lines = []
+
+File.readlines('/Users/yegor/Downloads/leads.csv').each do |row|
+  line = row.strip
+  next if line.empty?
   date, id, name, twitter, email = line.split(',')
-  puts "#{name.strip},#{email.strip}"
+  address = email.strip.downcase
+  if !emails.include? address
+    lines.push("#{name.strip},#{address}")
+    emails.push(address)
+  end
 end
+
+list = '/code/home/subscribers.txt'
+
+File.readlines(list).each do |row|
+  line = row.strip
+  next if line.empty?
+  name, email = line.split(',')
+  address = email.strip.downcase
+  if !emails.include? address
+    lines.push("#{name.strip},#{address}")
+    emails.push(address)
+  end
+end
+
+File.open(list, 'w') do |f|
+  lines.sort.each do |line|
+    f << "#{line}\n"
+  end
+end
+
+puts "#{lines.size} addresses saved to #{list}"
