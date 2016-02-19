21475fab65ccb7fe94d836e91afb9f120f3d43a5
Yegor Bugayenko <yegor@tpc2.com>
2016-02-19 15:07:57 -0800

    layout

--- _plugins/book.rb
+++ _plugins/book.rb
@@ -4,7 +4,7 @@ module Yegor
       opts = book.strip.split(/\s+/, 2)
       "<aside class='book'>" \
       "<a href='http://www.amazon.com/gp/product/1519166915/ref=as_li_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=1519166915&linkCode=as2&tag=yegor256com-20&linkId=RMGEO4ESPM4CGD3O'>" \
-      "<img src='/images/books/elegant-objects/cover.png' style='height:200px;float:left;margin-right:1em;'>" \
+      "<img src='/images/books/elegant-objects/cover.png'>" \
       "</a>" \
       "Read more about this problem in Section&nbsp;#{opts[1]} of this book" \
       "</aside>"
