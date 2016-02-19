6441eb6b405fbf7e108abfb3db4d75272a767dd4
Yegor Bugayenko <yegor@tpc2.com>
2016-02-19 14:46:23 -0800

    format

--- _plugins/book.rb
+++ _plugins/book.rb
@@ -4,9 +4,9 @@ module Yegor
       opts = book.strip.split(/\s+/, 2)
       "<aside class='book'>" \
       "<a href='http://www.amazon.com/Elegant-Objects-Yegor-Bugayenko/dp/1519166915/'>" \
-      "<img src='/images/books/elegant-objects/cover.png' style='width:50px;float:left;margin-right:1em;'>" \
+      "<img src='/images/books/elegant-objects/cover.png' style='height:200px;float:left;margin-right:1em;'>" \
       "</a>" \
-      "Read more about this problem in Section #{opts[1]} of this book..." \
+      "Read more about this problem in Section&nbsp;#{opts[1]} of this book" \
       "</aside>"
     end
   end
