df9b847d3c2a737093fc5f6bec69412161555a99
Yegor Bugayenko <yegor@tpc2.com>
2016-02-20 16:09:29 -0800

    amazon buy button

--- _plugins/book.rb
+++ _plugins/book.rb
@@ -5,11 +5,12 @@ module Yegor
       link = 'http://www.amazon.com/gp/product/1519166915/ref=as_li_tl?ie=UTF8&camp=1789&creative=390957&creativeASIN=1519166915&linkCode=as2&tag=yegor256com-20&linkId=RMGEO4ESPM4CGD3O'
       "<aside class='book'>" \
       "<a href='#{link}'>" \
-      "<img src='/images/books/elegant-objects/cover.png'>" \
+      "<img src='/images/books/elegant-objects/cover.png' class='book-cover'/>" \
       "</a>" \
       "Read more about this subject in " \
       "<a href='#{link}'>Section&nbsp;#{opts[1]}</a>" \
       " of this book" \
+      "<a href='#{link}'><img src='/images/books/amazon-buy-button.png' class='amazon-button'/></a>" \
       "</aside>"
     end
   end
