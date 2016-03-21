16e697b40a28753ff72ab2ebb25bf215dc030dad
Yegor Bugayenko <yegor@tpc2.com>
2016-03-20 20:42:20 -0700

    br

--- _plugins/book.rb
+++ _plugins/book.rb
@@ -8,7 +8,7 @@ module Yegor
       "<img src='/images/books/elegant-objects/cover.png' class='book-cover'/>" \
       "</a>" \
       "Read more about this subject in " \
-      "<a href='#{link}'>Section&nbsp;#{opts[1]}</a>" \
+      "<a href='#{link}'>Section&nbsp;#{opts[1]}</a><br/>" \
       " of my book" \
       "<a href='#{link}'><img src='/images/books/amazon-buy-button.png' class='amazon-button'/></a>" \
       "</aside>"
