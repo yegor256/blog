dd803fba93eb36ec21e9470e3f4ea6c8b5bc2622
Yegor Bugayenko <yegor@teamed.io>
2016-04-13 22:14:01 +0300

    url

--- _travis/install_cmake.sh
+++ _travis/install_cmake.sh
@@ -2,7 +2,7 @@
 set -e
 set -o pipefail
 
-wget --no-check-certificate http://www.cmake.org/files/v3.5/cmake-3.5.1.tar.gz
+wget --no-check-certificate https://cmake.org/files/v3.5/cmake-3.5.1.tar.gz
 tar xf cmake-3.5.1.tar.gz
 cd cmake-3.5.1
 ./configure
