eb50bc19984d9fad60a02dd8dd7ecbcd927a1bde
Yegor Bugayenko <yegor@teamed.io>
2016-04-13 20:57:18 +0300

    cmake 3.5.1

--- _travis/install_cmake.sh
+++ _travis/install_cmake.sh
@@ -2,10 +2,10 @@
 set -e
 set -o pipefail
 
-wget --no-check-certificate http://www.cmake.org/files/v3.2/cmake-3.2.2.tar.gz
-tar xf cmake-3.2.2.tar.gz
-cd cmake-3.2.2
+wget --no-check-certificate http://www.cmake.org/files/v3.5/cmake-3.5.1.tar.gz
+tar xf cmake-3.5.1.tar.gz
+cd cmake-3.5.1
 ./configure
 make
 sudo make install
-cd ..; rm -rf cmake-3.2.2
+cd ..; rm -rf cmake-3.5.1
