84eacabd015963a81307b8e583e4af0927665503
Yegor Bugayenko <yegor@teamed.io>
2016-07-21 19:27:18 -0700

    all.js instead of two files

--- js/all.js
+++ js/all.js
@@ -96,3 +96,17 @@ $(
     );
   }
 );
+
+$(
+  function() {
+    'use strict';
+    $('.main p a, .main ul a, .main ol a').each(
+      function () {
+        var $this = $(this), domain = new URL(this.href).hostname;
+        if (domain && domain !== window.location.hostname) {
+          $this.append('<i class="icon icon-exit foreign"></i>');
+        }
+      }
+    );
+  }
+);
