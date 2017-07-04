982b504d1548f32bf188e6a4380785882f7ce170
yegor256 <yegor256@gmail.com>
2017-07-04 09:56:45 +0300

    cache tidy and sfntwoof

--- _travis/install_sfnt2woff.sh
+++ _travis/install_sfnt2woff.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-wget http://img.teamed.io/woff-code-latest.zip
-unzip woff-code-latest.zip -d sfnt2woff
-cd sfnt2woff
-make
-mv sfnt2woff /usr/local/bin/
-cd ..
-rm -rf sfnt2woff
+if [ -e sfnt2woff ]; then
+  cd sfnt2woff
+else
+  wget http://img.teamed.io/woff-code-latest.zip
+  unzip woff-code-latest.zip -d sfnt2woff
+  cd sfnt2woff
+  make
+fi
+cp sfnt2woff /usr/local/bin/
--- _travis/install_tidy.sh
+++ _travis/install_tidy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/htacg/tidy-html5.git
-cd tidy-html5/build/cmake
-git checkout 5.1.25
-cmake ../..
-make
+if [ -e tidy-html5 ]; then
+  cd tidy-html5/build/cmake
+else
+  git clone https://github.com/htacg/tidy-html5.git
+  cd tidy-html5/build/cmake
+  git checkout 5.1.25
+  cmake ../..
+  make
+fi
 make install
-cd ../../..
-rm -rf tidy-html5
