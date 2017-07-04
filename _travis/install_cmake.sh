533cf1b55594212e49d1b4a18ab3907f485df578
yegor256 <yegor256@gmail.com>
2017-07-04 09:58:35 +0300

    cache cmake-3.5.1

--- _travis/install_cmake.sh
+++ _travis/install_cmake.sh
@@ -2,10 +2,13 @@
 set -e
 set -o pipefail
 
-wget --no-check-certificate https://cmake.org/files/v3.5/cmake-3.5.1.tar.gz
-tar xf cmake-3.5.1.tar.gz
-cd cmake-3.5.1
-./configure
-make
+if [ -e cmake-3.5.1 ]; then
+  cd cmake-3.5.1
+else
+  wget --no-check-certificate https://cmake.org/files/v3.5/cmake-3.5.1.tar.gz
+  tar xf cmake-3.5.1.tar.gz
+  cd cmake-3.5.1
+  ./configure
+  make
+fi
 make install
-cd ..; rm -rf cmake-3.5.1
