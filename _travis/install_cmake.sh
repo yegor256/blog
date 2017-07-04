d95f542b397470394cae99f860435d097302e657
yegor256 <yegor256@gmail.com>
2017-07-04 10:04:46 +0300

    typo

--- _travis/install_cmake.sh
+++ _travis/install_cmake.sh
@@ -2,7 +2,7 @@
 set -e
 set -o pipefail
 
-if [ -e cmake-3.5.1 ]; then
+if [ -e cmake-3.5.1/configure ]; then
   cd cmake-3.5.1
 else
   wget --no-check-certificate https://cmake.org/files/v3.5/cmake-3.5.1.tar.gz
