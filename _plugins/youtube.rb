fa28dc986d04fd2a167706b1a7b232484e74db8f
Yegor Bugayenko <yegor256@gmail.com>
2018-03-26 09:05:29 +0300

    width

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -33,12 +33,12 @@ module Yegor
       json = JSON.parse(Net::HTTP.get(uri))
       item = json['items'][0]
       snippet = item['snippet']
-      "<div class='youtube'>
+      "<aside class='youtube'>
         <a href='https://www.youtube.com/watch?v=#{@id}'>
         <img src='#{snippet['thumbnails']['medium']['url']}'>
         <i class='icon icon-play'></i></img></a>
         <div>#{snippet['title']};
-        #{Time.parse(snippet['publishedAt']).strftime('%-d %B %Y')}.</div></div>"
+        #{Time.parse(snippet['publishedAt']).strftime('%-d %B %Y')}.</div></aside>"
         # "#{item['statistics']['viewCount']} views; #{item['statistics']['likeCount']} likes"
     end
   end
