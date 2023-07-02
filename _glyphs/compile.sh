ed1dd90cb2d0955a596bb7204f52cb6bc8c1f96a
Yegor Bugayenko <yegor256@gmail.com>
2023-07-02 13:49:51 +0300

    debug

--- _glyphs/compile.sh
+++ _glyphs/compile.sh
@@ -13,5 +13,5 @@ target=$1
 mkdir -p "${target}"
 fontcustom compile "${dir}" \
   "--output=${target}" \
-  --font-name=icons --templates=css --no-hash --force --autowidth
+  --font-name=icons --templates=css --no-hash --force --autowidth --debug
 cat "${target}/icons.css" | sed -E "s|\"./icons([^\"]+)\"|\"https://www.yegor256.com/css/icons\1?$(date +%s)\"|g" > "${target}/icons.css"
