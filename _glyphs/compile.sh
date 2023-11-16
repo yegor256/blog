0d819cca1f3259e86f0bcfc44512d107562bd11b
Yegor Bugayenko <yegor256@gmail.com>
2023-11-16 08:01:36 +0300

    #636 fontcustom alone

--- _glyphs/compile.sh
+++ _glyphs/compile.sh
@@ -2,16 +2,15 @@
 set -e
 set -x
 
-# we need this in order to avoid conflicts with Gemfile
-unset BUNDLE_GEMFILE
-cd /tmp
-
 dir=$(dirname "$0")
 
 target=$1
 
 mkdir -p "${target}"
+
 fontcustom compile "${dir}" \
   "--output=${target}" \
   --font-name=icons --templates=css --no-hash --force --autowidth --debug
+
 cat "${target}/icons.css" | sed -E "s|\"./icons([^\"]+)\"|\"https://www.yegor256.com/css/icons\1?$(date +%s)\"|g" > "${target}/icons.css"
+
