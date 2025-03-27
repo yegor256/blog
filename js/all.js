8b0bf43eed21b00a750fb8291a2ae06fa499fc8f
Yegor Bugayenko <yegor256@gmail.com>
2025-03-27 14:58:59 +0800

    cleanup

--- js/all.js
+++ js/all.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2014-2025 Yegor Bugayenko
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the 'Software'), to deal
 // in the Software without restriction, including without limitation the rights
@@ -7,7 +7,7 @@
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so. The Software doesn't include files with .md extension.
 // That files you are not allowed to copy, distribute, modify, publish, or sell.
-// 
+//
 // THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
