5e8808f39c70a46800fa3009571e16b77e727673
Yegor Bugayenko <yegor256@gmail.com>
2025-05-18 20:45:25 +0300

    eslint config

---
+++ .github/eslint.config.js
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: Copyright (c) 2014-2025 Yegor Bugayenko
+// SPDX-License-Identifier: MIT
+
+const { configs } = require('@eslint/js');
+
+module.exports = [
+  {
+    ignores: ['dist/', 'node_modules/']
+  },
+  {
+    ...configs.all,
+    files: ['assets/js/**.js'],
+    languageOptions: {
+      ecmaVersion: 2019,
+      sourceType: 'module'
+    },
+    rules: {
+      ...configs.all.rules,
+      'sort-vars': 'off',
+      'capitalized-comments': 'off',
+      'no-alert': 'off',
+      'no-magic-numbers': 'off',
+      'indent': ['error', 2],
+      'max-len': ['error', { code: 100 }]
+    }
+  }
+];
