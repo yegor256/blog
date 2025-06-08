e8711b9e9fc369c74c45fbe8dd9c3ddcc81d2247
Yegor Bugayenko <yegor256@gmail.com>
2025-06-08 11:49:46 +0300

    #798: Add external link icon to external links.

--- js/all.js
+++ js/all.js
@@ -32,6 +32,13 @@ if (typeof($) != 'undefined') {
       }
       return num;
     }
+    $('article a[href]').each(function () {
+      const href = $(this).attr('href');
+      if (href.startsWith('http') && !href.includes(location.hostname)) {
+        $(this).addClass('external');
+        $(this).append('<img src="/images/icons/link.svg" class="link" alt="external"/>');
+      }
+    });
     if ($('.buttons').css('display') !== 'none') {
       $(".button[href!='#']").click(
         function (event) {
