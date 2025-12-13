6463b88c6a35d3dde40272e89efbd2222a1c05f2
yegor256 <yegor256@gmail.com>
2025-12-13 06:09:41 +0000

    Deploying to gh-pages from @ yegor256/blog@617e4e5b88b39e53660b9b7cd82913e539f136c9 🚀

--- js/all.js
+++ js/all.js
@@ -5,9 +5,13 @@
 
 /*globals $:false, window:false, document:false */
 
+// This is Disqus configuration, to make it identify the website correctly.
 var disqus_domain = 'yegor256.com';
 var disqus_shortname = 'yegor256';
-this.page.identifier = location.origin + location.pathname;
+var disqus_config = function() {
+  this.page.url = location.origin + location.pathname;
+  this.page.identifier = location.origin + location.pathname;
+};
 
 var VK = {};
 if (typeof($) != 'undefined') {
