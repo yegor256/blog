364d5f3498314b9eba595f3a999702776ad05d36
Yegor Bugayenko <yegor256@gmail.com>
2026-07-20 11:35:00 +0300

    Refactor badge letter to 'M' instead of 'L'

--- _plugins/jttu.rb
+++ _plugins/jttu.rb
@@ -15,7 +15,7 @@ module Yegor
     def render(context)
       Yegor::Badge.new(
         "https://github.com/yegor256/bibliography/blob/master/book-covers/#{@name}.jpg?raw=true",
-        'L',
+        'M',
         "https://jttu.net/#{@name}"
       ).render(context)
     end
